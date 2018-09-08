509c6ed12e0fab59a1df3ed7d4df4e8ac156214b
Aaron Sletten <aaron.j.sletten@gmail.com>
2018-09-08 17:25:29 -0500

    heroku route test

--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('about', { title: 'Aaron Sletten', condition: true, anyArray: [1,2,3] });
+  res.render('about', { title: 'Aaron Sletten'});
 });
 
 router.get('/resume', function(req, res, next) {
-  res.render('resume', { title: 'Resume', condition: true, anyArray: [1,2,3] });
+  res.render('resume', { title: 'Resume'});
 });
 
 router.get('/interests', function(req, res, next) {
-  res.render('interests', { title: 'Interests', condition: true, anyArray: [1,2,3] });
+  res.render('interests', { title: 'Interests'});
 });
 
 //Projects
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects', condition: true, anyArray: [1,2,3] });
+  res.render('projects', { title: 'Projects'});
 });
 
+//Lab monitor
 router.get('/projects/lab_monitor', function(req, res, next) {
-  res.render('projects/lab_monitor', { title: 'Lab Monitor Website', condition: true, anyArray: [1,2,3] });
+  res.render('test/lab_monitor', { title: 'Lab Monitor Website'});
 });
 
 
